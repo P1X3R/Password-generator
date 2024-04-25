3e1a7704d502659c242d02421d006ea1e05174ad
P1x3r <justthepixuser@hotmail.com>
2024-04-25 20:47:18 +0000

    Now you can select the password length

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
-const ALLOWED_CHARS: &str = "hBsjPJa5Llm";
+const ALLOWED_CHARS: &str = "jGcA8g2v=q/D";
 
 fn main() {
+    // Read the input
+    let mut password_length = String::new();
+
+    println!("Enter the password length: ");
+    std::io::stdin().read_line(&mut password_length).unwrap();
+    let password_length: usize = password_length.trim().parse().unwrap();
+
     // Get the hour in ms
     let start = SystemTime::now();
     let since_the_epoch = start
@@ -12,12 +19,24 @@ fn main() {
 
     // Generate random big number
     let mut random_number = String::new();
+    let mut operation: &str = "multiply";
 
-    for _ in 0..16 {
+    for _ in 0..password_length {
         let state = (seed >> 1) | ((seed & 1) ^ (seed << 1 & 1)) << 15; // Implementation of LSFR state
 
         // Add all the state, because if I add just the last, always returns 0
-        random_number += (state*2).to_string().as_str();
+        if operation == "multiply" {
+            random_number += (state*2).to_string().as_str();
+            operation = "divition";
+        }
+        else if operation == "divition" {
+            random_number += (state/2).to_string().as_str();
+            operation = "power";
+        }
+        else if operation == "power" {
+            random_number += (state*2*2*2).to_string().as_str();
+            operation = "multiply";
+        }
     }
 
     let mut result = String::new();
@@ -27,5 +46,6 @@ fn main() {
         result += ALLOWED_CHARS.chars().nth(number).unwrap().to_string().as_str();
     }
 
-    println!("{result}");
+    let result = &(result.as_str())[..password_length];
+    println!("Result: {result}");
 }
